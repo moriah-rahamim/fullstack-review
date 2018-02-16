f1e60809abac20f5d39b3921a27d59fbd231e193
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-16 15:38:02 -0800

    fix bug in Search.jsx for term and this bindings

--- database/index.js
+++ database/index.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  html_url: String, // repository url
+  description: String,
+  created_at: Date,
+  updated_at: Date,
+
+  owner_id: Number,
+  owner_login: String, // username
+  avatar_url: String,
+  owner_html_url: String // user profile url
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
